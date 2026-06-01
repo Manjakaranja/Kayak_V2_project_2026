e365f574ae007892c6360514423a9ca6656d6126
Manjakaranja <manjakaranja@gmail.com>
2026-06-01 16:48:01 +0200

    add files v3

--- start.sh
+++ start.sh
@@ -8,8 +8,5 @@ airflow db migrate
 echo "Starting scheduler..."
 airflow scheduler &
 
-echo "Starting triggerer..."
-airflow triggerer &
-
 echo "Starting API server on port 8080..."
 exec airflow api-server --port 8080 --hostname 0.0.0.0
